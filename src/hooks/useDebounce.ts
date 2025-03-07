51868223732e06eec545ae421bf60bed45e43073
DonggunLim <Ldonggun6766@gmail.com>
2025-03-07 11:02:05 +0900

    fix: useDebounce unmount에 timeoute clear 추가

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,7 +1,15 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 const useDebounce = (cb: () => void, time = 500) => {
   const timer = useRef<number>(null);
+
+  useEffect(() => {
+    return () => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+      }
+    };
+  }, []);
 
   return useCallback(() => {
     if (timer.current) {
