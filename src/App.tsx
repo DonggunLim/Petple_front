3175c382fdf23096df21778145b62146793e180c
DonggunLim <Ldonggun6766@gmail.com>
2025-04-22 12:10:41 +0900

    chore: i analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Routes } from "./components";
 import { ToastContainer } from "./components/UI/Toast/components";
 import { HelmetProvider } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/react";
 export const queryClient = new QueryClient();
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
       {import.meta.env.MODE === "development" && (
         <ReactQueryDevtools initialIsOpen={false} />
       )}
+      <Analytics />
     </QueryClientProvider>
   );
 };
