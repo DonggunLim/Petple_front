8e783f2a2f9bdd5bc0a089db802522821dc19453
DonggunLim <Ldonggun6766@gmail.com>
2025-05-01 14:07:07 +0900

    style: chat header 상단 고정

---
+++ src/pages/Chat/components/ChatHeader.tsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./ChatHeader.module.css";
+import { UserType } from "@/types/user.type";
+
+interface ChatHeaderProps {
+  targetUser: UserType;
+}
+
+const ChatHeader = ({ targetUser }: ChatHeaderProps) => {
+  const navigate = useNavigate();
+  return (
+    <div className={styles.chat_header}>
+      <div
+        className={styles.back_button}
+        onClick={() => navigate("/petfriends")}
+      >
+        <img src="/images/prev.png" alt="뒤로가기 버튼 아이콘" />
+      </div>
+      <h2>{targetUser.nickName}님 과의 대화방</h2>
+    </div>
+  );
+};
+
+export default ChatHeader;
--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -4,13 +4,13 @@ import ChatList from "./components/ChatList";
 import useChatSocket from "./hooks/useChatSocket";
 import userAuthStore from "@/zustand/userAuth";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { getPrevMessages } from "@/apis/messages.api";
 import { Helmet } from "react-helmet-async";
+import ChatHeader from "./components/ChatHeader";
 
 const ChatPage = () => {
   const { nickname: targetUserNickname } = useParams();
-  const navigate = useNavigate();
   const signinedUser = userAuthStore();
   const {
     data: { targetUser, chat },
@@ -34,17 +34,7 @@ const ChatPage = () => {
             content={`${targetUser?.nickname}님과 실시간 채팅을 즐겨보세요.!`}
           />
         </Helmet>
-        <div className={styles.chat_header}>
-          <div
-            className={styles.back_button}
-            onClick={() => navigate("/petfriends")}
-          >
-            <img src="/images/prev.png" alt="뒤로가기 버튼 아이콘" />
-          </div>
-          <h1>
-            {targetUser.userPet[0]?.name ?? targetUser.nickName}님 과의 대화방
-          </h1>
-        </div>
+        <ChatHeader targetUser={targetUser} />
         <ChatList messages={messages} signinedUser={signinedUser} />
         <ChatInput sendMessage={sendMessage} />
       </div>
